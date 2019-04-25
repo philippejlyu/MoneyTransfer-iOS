d573f5700bfb1989b4f8d11d744346ab3219104f
Philippe Yu <philippejlyu@gmail.com>
2019-04-24 18:45:04 -0700

    Initial Commit

---
+++ MoneyTransfer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyTransfer
+//
+//  Created by Philippe Yu on 4/24/19.
+//  Copyright © 2019 Philippe Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
